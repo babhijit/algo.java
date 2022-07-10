3345906d3a0d29c5c3d95d058149ede089a9f3f9
babhijit <abhijitb@gmail.com>
2022-07-11 00:05:05 +0530

    746. min_cost_climbing_stairs DP

---
+++ src/test/java/com/algo/leetcode/dp/min_cost_climbing_stairs_746/TestCase.java
@@ -0,0 +1,11 @@
+package com.algo.leetcode.dp.min_cost_climbing_stairs_746;
+
+public class TestCase {
+    public final int[] cost;
+    public final int output;
+
+    public TestCase(int[] cost, int output) {
+        this.cost = cost;
+        this.output = output;
+    }
+}
